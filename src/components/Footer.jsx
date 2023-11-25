9884c73e01f4e934f426ad6787d390feebc51589
sal400 <flarebd@gmail.com>
2023-11-26 00:45:05 +0600

    header and footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import { IoChatbubblesOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+    return (
+        <div className="footer justify-between py-10">
+            <aside className="flex flex-col justify-center h-full">
+                <h2 className="text-3xl font-semibold flex items-center gap-2"><IoChatbubblesOutline/>QUICKPOST</h2>
+                <p>QUICKPOST - Online Conversation Platform</p>
+            </aside>
+            <nav>
+                <header className="footer-title">Services</header>
+                <Link to={'/manage'} className="link link-hover">Manage Forum</Link>
+                <a className="link link-hover">Design</a>
+                <a className="link link-hover">Marketing</a>
+                <a className="link link-hover">Advertisement</a>
+            </nav>
+            <nav>
+                <header className="footer-title">Company</header>
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Jobs</a>
+                <a className="link link-hover">Press kit</a>
+            </nav>
+            <nav>
+                <header className="footer-title">Legal</header>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,59 @@
+import { Link, NavLink } from "react-router-dom";
+import { IoMdNotifications } from "react-icons/io";
+import { IoChatbubblesOutline } from "react-icons/io5";
+
+const Header = () => {
+    const user = true
+
+    const handleSignout = () => {
+        console.log('logout')
+    }
+
+    //Navlink
+    const links = <>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/">Membership</NavLink></li>
+        <li><NavLink to="/"><IoMdNotifications/></NavLink></li>
+        <li><NavLink to="/register">Join Us</NavLink></li>
+    </>
+    const linksPrivate = <>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/">Membership</NavLink></li>
+        <li><NavLink to="/"><IoMdNotifications/></NavLink></li>
+    </>
+
+    return (
+        <div className="py-4">
+            {/* Logo & Button */}
+            <div className="flex flex-col md:flex-row md:gap-10 items-center justify-between mb-2">
+                <div>
+                    <Link to="/" className="text-3xl font-semibold flex items-center gap-2"><IoChatbubblesOutline/>QUICKPOST</Link>
+                </div>
+                <div className="mt-6 md:mt-0">
+                    <div>
+                        <ul className={`flex items-center gap-4 font-semibold list-none uppercase`}>
+                            {user ?
+                                <>
+                                    {linksPrivate}
+                                    <div className="dropdown dropdown-hover">
+                                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                                            <div className="w-10 rounded-full">
+                                                <img src={user.photoURL} />
+                                            </div>
+                                        </label>
+                                        <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded">
+                                            <li className="whitespace-nowrap">{user.displayName}</li>
+                                            <li><Link onClick={handleSignout} className="navbtn rounded-lg">Signout</Link></li>
+                                        </ul>
+                                    </div>
+                                </> :
+                                links}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
