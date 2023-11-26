7fe551b5294af61a8c1ddb15cd7e490ebd733f7b
sal400 <flarebd@gmail.com>
2023-11-26 18:48:20 +0600

    tags section ui implemented to search banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,8 +3,9 @@ import { Link } from "react-router-dom";
 const Banner = () => {
     return (
         <div className="bg-cover bg-center">
-            <div className="bg-gray rounded-lg lg:flex lg:justify-center lg:items-center">
-                <div className="text-center p-6 md:pt-32 md:pb-52">
+            <div className="bg-gray rounded-lg flex justify-center items-center">
+                <div className="text-center p-6 md:p-32">
+                    {/* Search Box */}
                     <h3 className="text-5xl font-bold mb-10">Search Posts</h3>
                     <label>
                         <div className="bg-white border border-search max-w-md mx-auto flex justify-between rounded-lg">
@@ -12,6 +13,20 @@ const Banner = () => {
                             <Link to="/err"><button className="bg-green text-white py-4 px-7 rounded-r-lg">Search</button></Link>
                         </div>
                     </label>
+
+                    {/* Tags */}
+                    <ul className="tags pt-8 md:pt-10 gap-5 flex flex-wrap justify-center max-w-xl">
+                        <li>list1</li>
+                        <li>list2</li>
+                        <li>list3</li>
+                        <li>list4</li>
+                        <li>list5</li>
+                        <li>list6</li>
+                        <li>list7</li>
+                        <li>list8</li>
+                        <li>list9</li>
+                        <li>list10</li>
+                    </ul>
                 </div>
             </div>
         </div>
