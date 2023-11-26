6ab7384f18cf968d619a425c7d82bb07a15c1abc
sal400 <flarebd@gmail.com>
2023-11-26 14:58:28 +0600

    header dropdown fixt

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -42,7 +42,7 @@ const Header = () => {
                             {user ?
                                 <>
                                     {linksPrivate}
-                                    <div className="dropdown dropdown-hover">
+                                    <div className="dropdown dropdown-end dropdown-hover">
                                         <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                                             <div className="w-10 rounded-full">
                                                 <img src={user.photoURL} />
