b9ee97326384a039b9eb6b95dc78d4b42e84fa8f
sal400 <flarebd@gmail.com>
2023-12-02 03:46:46 +0600

    replaced banner, tag & feed component with direct jsx code

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,71 @@
+import { useEffect, useState } from "react";
 import Announcement from "../components/Announcement";
-import Banner from "../components/Banner";
-import Feed from "../components/Feed";
-import Tag from "../components/Tag";
+import { axiosBase } from "../hooks/useAxios";
+import Post from "../components/Post";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        axiosBase.get('/posts')
+            .then(res => {
+                setPosts(res.data)
+            })
+    }, [])
+
     return (
         <div>
-            <Banner></Banner>
-            <Tag></Tag>
+            {/* Banner */}
+            <div className="bg-cover bg-center">
+                <div className="bg-gray rounded-lg flex justify-center items-center">
+                    <div className="text-center p-6 md:p-10 w-full">
+                        {/* Search Box */}
+                        <h3 className="text-3xl font-semibold mb-6 uppercase">Search Posts</h3>
+                        <label>
+                            <div className="bg-white border border-search max-w-5xl mx-auto flex justify-between rounded-lg">
+                                <input className="focus:outline-none m-4" type="text" name="search" placeholder="Search here...." />
+                                <Link to="/err"><button className="bg-green text-white py-4 px-7 rounded-r-lg">Search</button></Link>
+                            </div>
+                        </label>
+                    </div>
+                </div>
+            </div>
+
+            {/* Tags */}
+            <div className="bg-gray my-6 rounded-lg text-center font-semibold">
+                <div className="p-5">
+                    <h3 className="text-xl font-semibold mb-2 uppercase">Tags</h3>
+                    <ul className="tags gap-5 flex flex-wrap justify-center max-w-5xl mx-auto">
+                        <Link to={'/tags/help'}><li>help</li></Link>
+                        <Link to={'/tags/suggestion'}><li>suggestion</li></Link>
+                        <Link to={'/tags/feedback'}><li>feedback</li></Link>
+                    </ul>
+                </div>
+            </div>
+
+            {/* Announcement */}
             <Announcement></Announcement>
-            <Feed></Feed>
+
+            {/* Feed */}
+            <div className="bg-gray my-6 rounded-lg">
+                <div className="p-5">
+
+                    <div className="flex justify-center m-4 gap-4 font-semibold uppercase">
+                        <h3>Sort by Popularity:</h3>
+                        <select name="popularity" className="rounded">
+                            <option value="desc">Highest - Lowest</option>
+                            <option value="asc">Lowest - Highest</option>
+                        </select>
+                    </div>
+
+                    <ul className="list-decimal list-inside max-w-5xl mx-auto">
+                        {
+                            posts.map(x => <Post key={x._id} obj={x}></Post>)
+                        }
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
