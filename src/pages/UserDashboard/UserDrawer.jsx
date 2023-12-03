ba811141af54ce8a8c795b9e167e324286c9a88d
sal400 <flarebd@gmail.com>
2023-12-04 01:39:22 +0600

    UserDrawer junk code removed

--- src/pages/UserDashboard/UserDrawer.jsx
+++ src/pages/UserDashboard/UserDrawer.jsx
@@ -26,21 +26,4 @@ const UserDrawer = () => {
     );
 };
 
-export default UserDrawer; <div className="drawer lg:drawer-open">
-    <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-    <div className="drawer-content flex flex-col items-center justify-center">
-        {/* Page content here */}
-        <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-
-    </div>
-    <div className="drawer-side">
-        <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay">Dashboard</label>
-        <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-            {/* Sidebar content here */}
-            <li><Link to={'/profile'}>My Profile</Link></li>
-            <li><Link to={'/add'}>Add Post</Link></li>
-            <li><Link to={'/posts'}>My Posts</Link></li>
-        </ul>
-
-    </div>
-</div>
+export default UserDrawer;
